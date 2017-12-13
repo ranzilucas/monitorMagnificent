0082df41226969a74929efe66d08142c260eee68
Lucas Ranzi <lucasr@dbserver.com.br>
2017-12-13 18:41:27 -0200

    create project

---
+++ src/main/java/br/ranzi/monitor/monitorMagnificent/MonitorMagnificentApplication.java
@@ -0,0 +1,12 @@
+package br.ranzi.monitor.monitorMagnificent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorMagnificentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorMagnificentApplication.class, args);
+	}
+}
